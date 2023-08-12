b6de2bfcd88b65b55c5c7b378157f425ddd300a8
Lucas Marins <lucasbetim1213@gmail.com>
2023-08-12 03:44:27 -0300

    angular 16 = não funcionando ainda

--- template/demo_1/src/app/forms/form.module.ts
+++ template/demo_1/src/app/forms/form.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BasicElementsComponent } from './basic-elements/basic-elements.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { CustomFormsModule } from 'ng2-validation';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
@@ -17,7 +16,6 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     NgSelectModule,
     NgbModule,
-    CustomFormsModule,
   ],
   providers: []
 })
